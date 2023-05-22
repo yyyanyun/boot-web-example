ca4529805ea2d9f119e71a919439341f34430e4b
lsl <2091924566@qq.com>
2023-05-22 20:02:14 +0800

    添加了柜机添加的接口方法

--- qf-business/qf-cabinet/src/main/java/com/qf/cabinet/controller/CabinetController.java
+++ qf-business/qf-cabinet/src/main/java/com/qf/cabinet/controller/CabinetController.java
@@ -1,6 +1,8 @@
 package com.qf.cabinet.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.qf.cabinet.qo.CabinetEasyQo;
+import com.qf.cabinet.qo.CabinetHighQo;
 import com.qf.cabinet.qo.CabinetQo;
 import com.qf.cabinet.service.CabinetService;
 import com.qf.cabinet.vo.CabinetVo;
@@ -13,6 +15,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 import java.util.concurrent.CancellationException;
 
 @Api("快递柜机接口")
@@ -27,24 +30,23 @@ public class CabinetController {
      *
      * @param page:当前页
      * @param size：页量
-     * @param cabinetQo：请求参数对象
+     * @param cabinetEasyQo：请求参数对象
      * @param param：模糊匹配参数
      * @return ：IPage<CabinetVo>响应对象
      * @throws CancellationException： 控制层异常
      */
     @ApiImplicitParams({
             @ApiImplicitParam(name = "page", value = "当前页", paramType = "path", dataType = "java.lang.int"),
-            @ApiImplicitParam(name = "size", value = "页量", paramType = "path",dataType = "java.lang.int"),
-            @ApiImplicitParam(name = "cabinetQo", value = "简单请求参数对象", paramType = "query"),
+            @ApiImplicitParam(name = "size", value = "页量", paramType = "path", dataType = "java.lang.int"),
             @ApiImplicitParam(name = "param", value = "详细地址/编码/型号/版本号/ID", paramType = "query"),
     })
     @ApiOperation(value = "listSimple", tags = "简单搜索")
     @PostMapping("/listSimple/{page}/{size}")
     public IPage<CabinetVo> listSimple(@PathVariable @RequestParam(defaultValue = "1") int page,
                                        @PathVariable @RequestParam(defaultValue = "5") int size,
-                                       @RequestBody CabinetQo cabinetQo,
+                                       @RequestBody CabinetEasyQo cabinetEasyQo,
                                        String param) throws CancellationException {
-        return cabinetService.listSimple(page, size, cabinetQo, param);
+        return cabinetService.listSimple(page, size, cabinetEasyQo, param);
     }
 
     /**
@@ -52,7 +54,7 @@ public class CabinetController {
      *
      * @param page:当前页
      * @param size：页量
-     * @param cabinetQo：请求参数对象
+     * @param cabinetHighQo：请求参数对象
      * @return ：IPage<CabinetVo>响应对象
      * @throws CancellationException： 控制层异常
      */
@@ -61,17 +63,17 @@ public class CabinetController {
             @ApiImplicitParam(name = "size", value = "页量", paramType = "path", dataType = "java.lang.int"),
     })
     @ApiOperation(value = "list", tags = "高级搜索")
-    @PostMapping("/list/{page}/{size}")
+    @GetMapping("/list/{page}/{size}")
     public IPage<CabinetVo> list(@PathVariable @RequestParam(defaultValue = "1") int page,
                                  @PathVariable @RequestParam(defaultValue = "5") int size,
-                                 CabinetQo cabinetQo) throws CancellationException {
-        System.out.println(page+size);
-        return cabinetService.list(page, size, cabinetQo);
+                                 CabinetHighQo cabinetHighQo) throws CancellationException {
+        return cabinetService.list(page, size, cabinetHighQo);
     }
 
-    public RespResult<Integer> add(@RequestBody CabinetQo cabinetQo) throws ControllerException {
-
-return null;
+    @PostMapping("/add")
+    @ApiOperation(value = "add",tags = "添加柜机")
+    public RespResult<Integer> add(@RequestBody @Valid CabinetQo cabinetQo) throws ControllerException {
+        return RespResult.success(cabinetService.add(cabinetQo));
     }
 
 
