be94370e3a8ead1d128d22598870e81291ff1623
xiaowang <1738546154@qq.com>
2023-05-25 09:53:58 +0800

    1

--- qf-business/qf-helpcenter/src/main/java/com/qf/helpcenter/controller/ProblemClassificationController.java
+++ qf-business/qf-helpcenter/src/main/java/com/qf/helpcenter/controller/ProblemClassificationController.java
@@ -2,12 +2,12 @@ package com.qf.helpcenter.controller;
 
 import com.github.pagehelper.PageInfo;
 import com.qf.common.base.result.RespResult;
-import com.qf.helpcenter.entity.ProblemClassificationTable;
 import com.qf.helpcenter.qo.ProblemClassificationTableAddQo;
 import com.qf.helpcenter.qo.ProblemClassificationTableQo;
 import com.qf.helpcenter.server.ProblemClassificationServer;
 import com.qf.helpcenter.vo.ProblemClassificationTableVo;
 import com.qf.helpcenter.vo.SuggestedFeedbackVo;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -18,20 +18,23 @@ public class ProblemClassificationController {
 
     @Resource
     private ProblemClassificationServer problemClassificationServer;
+    @ApiOperation("查看问题分类")
     @GetMapping("/listp")
     public RespResult<PageInfo<ProblemClassificationTableVo>> showProblem(@RequestParam(defaultValue = "1") Integer page , @RequestParam(defaultValue = "10")Integer size){
         return RespResult.success(problemClassificationServer.showClassification(page, size));
     }
-
+    @ApiOperation("高级搜索")
     @PostMapping("/lists")
     public  RespResult<PageInfo<ProblemClassificationTableVo>>searchClassification(@RequestParam(defaultValue = "1") Integer page , @RequestParam(defaultValue = "10")Integer size,@RequestBody ProblemClassificationTableQo problemClassificationTableQo){
         return RespResult.success(problemClassificationServer.searchClassification(page, size, problemClassificationTableQo));
     }
+    @ApiOperation("添加分类")
     @PostMapping("/add")
     public RespResult addClassification(ProblemClassificationTableAddQo problemClassificationTableAddQo){
         return problemClassificationServer.addClassification(problemClassificationTableAddQo);
 
     }
+    @ApiOperation("更改状态")
     @PostMapping("/updateClassification")
     public  RespResult updateClassification(@RequestParam Integer id,@RequestParam Integer type){
         return problemClassificationServer.updateClassification(id, type);
--- qf-business/qf-helpcenter/src/main/java/com/qf/helpcenter/controller/SuggestedFeedbackController.java
+++ qf-business/qf-helpcenter/src/main/java/com/qf/helpcenter/controller/SuggestedFeedbackController.java
@@ -1,33 +1,35 @@
 package com.qf.helpcenter.controller;
-
 import com.github.pagehelper.PageInfo;
 import com.qf.common.base.result.RespResult;
 import com.qf.helpcenter.qo.SuggestedFeedbackAddQo;
 import com.qf.helpcenter.qo.SuggestedFeedbackQo;
 import com.qf.helpcenter.server.SuggestedFeedbackServer;
 import com.qf.helpcenter.vo.SuggestedFeedbackVo;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.Resource;
-
 @RequestMapping("/query")
 @RestController
 public class SuggestedFeedbackController {
     @Resource
     private SuggestedFeedbackServer suggestedFeedbackServer;
+    @ApiOperation("查询问题列表")
     @PostMapping ("/list")
     public RespResult<PageInfo<SuggestedFeedbackVo>> showProblem(@RequestParam(defaultValue = "1") Integer page , @RequestParam(defaultValue = "10")Integer size){
          return RespResult.success( suggestedFeedbackServer.showProblem(page, size));
     }
+    @ApiOperation("模糊查询和高级搜索")
     @PostMapping ("/lists")
     public RespResult<PageInfo<SuggestedFeedbackVo>> searchProblen( SuggestedFeedbackQo suggestedFeedbackQo,@RequestParam(defaultValue = "1") Integer page , @RequestParam(defaultValue = "10")Integer size){
         return RespResult.success( suggestedFeedbackServer.searchProblen(page,size,suggestedFeedbackQo));
     }
+    @ApiOperation("添加问题")
     @PostMapping ("/addproblem")
     public RespResult addProblen(SuggestedFeedbackAddQo suggestedFeedbackAddQo){
         return suggestedFeedbackServer.addProblem(suggestedFeedbackAddQo);
     }
 
+    @ApiOperation("更改状态")
     @PostMapping ("/updateproblem")
     public RespResult updateProblen(@RequestParam Integer id,@RequestParam Integer type){
         return suggestedFeedbackServer.updateProblem(id,type);
